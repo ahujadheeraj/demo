8809273b0e207f9b90a0a6cc4279093c758f6f96
Dheeraj Ahuja <ahujadheeraj99@gmail.com>
2019-08-16 20:02:51 +0530

    Updates

---
+++ precache-manifest.2ce0e250974d2d9c94993781092292b1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b91fc58cc2c630f6a3be68d9f2cc83e",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "34688b1d68450b3d8ec9",
+    "url": "/demo/static/css/main.66a36be8.chunk.css"
+  },
+  {
+    "revision": "1f1b921e849d0529afdd",
+    "url": "/demo/static/js/2.8c2c1fa3.chunk.js"
+  },
+  {
+    "revision": "34688b1d68450b3d8ec9",
+    "url": "/demo/static/js/main.76fabff5.chunk.js"
+  },
+  {
+    "revision": "017d411d22a8b38f53de",
+    "url": "/demo/static/js/runtime~main.9b4820ef.js"
+  },
+  {
+    "revision": "f1be2b4434e6fd27b8b154f572661dd7",
+    "url": "/demo/static/media/adt.f1be2b44.svg"
+  },
+  {
+    "revision": "117f84ae4e4e677a186fede023480ff8",
+    "url": "/demo/static/media/enterprise.117f84ae.jpg"
+  },
+  {
+    "revision": "7c01bfc046a029227993df347594bc58",
+    "url": "/demo/static/media/flipkart.7c01bfc0.svg"
+  },
+  {
+    "revision": "ba7c90949732d5bc7e63fed76324c733",
+    "url": "/demo/static/media/growth.ba7c9094.jpg"
+  },
+  {
+    "revision": "77d9431cbbe2c17b303a62efcfc17d20",
+    "url": "/demo/static/media/honda.77d9431c.svg"
+  },
+  {
+    "revision": "e19de7ae92c716fac12cd45b02ff8c3a",
+    "url": "/demo/static/media/playment.e19de7ae.svg"
+  },
+  {
+    "revision": "2f6134e650c1cc1b374b00bd3eb71cfc",
+    "url": "/demo/static/media/sales.2f6134e6.jpg"
+  },
+  {
+    "revision": "bf876d81f291406719fc6adcc006eab9",
+    "url": "/demo/static/media/yourstory.bf876d81.svg"
+  }
+]);
